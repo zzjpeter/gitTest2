630e4f6bf1f8dc87a917aee0ad7c058a0c73ec8e
GotGitHub <gotgithub@gmail.com>
2017-03-10 00:24:49 +0800

    add gitTest2

---
+++ gitTest2/gitTest2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitTest2
+//
+//  Created by 朱志佳 on 17/3/10.
+//  Copyright © 2017年 zzj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitTest2/gitTest2/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  gitTest2
+//
+//  Created by 朱志佳 on 17/3/10.
+//  Copyright © 2017年 zzj. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
